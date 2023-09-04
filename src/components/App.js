0bb47c159f2ef1dbb7cb032d1e3267ce0fa2cd60
IsaacDJohnson <isaac.designws@gmail.com>
2023-09-04 11:18:47 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar github={user.links.github} linkedin={user.links.linkedin}/>
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links}/>
     </div>
   );
 }
